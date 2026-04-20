97299b4708c8a4e5518ca78af8d5f9871d62894a
AdriSos <adri.gonz.2004s@gmail.com>
2026-04-20 17:04:27 -0600

    Se modifico el archivo UserController

--- src/main/java/com/auramusic/backend/controller/UserController.java
+++ src/main/java/com/auramusic/backend/controller/UserController.java
@@ -3,13 +3,17 @@ package com.auramusic.backend.controller;
 import com.auramusic.backend.model.User;
 import com.auramusic.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin(origins = "*")
 public class UserController {
-    @Autowired private UserRepository userRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/{email}")
     public User getUserByEmail(@PathVariable String email) {
